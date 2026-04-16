ab02081054dfc1d262d9ca6865f97906cb9c538e
Jason Belmonti <jasonbelmonti@gmail.com>
2026-04-15 21:44:16 -0500

    implement markdown body section parsing

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export type * from "./src/core-model/index.ts";
 export * from "./src/document-discovery/index.ts";
+export * from "./src/markdown-body/index.ts";
 export * from "./src/profile-registry/index.ts";
